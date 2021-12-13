1a671cdac8839dea6dec22ff59c0071ca6acc0b6
Antoine Vignon <vignonantoinem@gmail.com>
2021-12-13 00:34:49 -0500

    Replaced react-html-parser with html-react-parser

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 import React, { useEffect, useState } from 'react';
-//import Parser from "react-html-parser";
+import Parser from "html-react-parser";
 
 const displayChart = require('./displayChart');
 
@@ -165,7 +165,7 @@ export default function HandleInputForm () {
               onChange={handleChange}
               required
             >
-              {result }
+              {Parser(result)}
             </select><br></br>
             <label htmlFor='sensor-id2' className='pr-2 font-medium'> Second Sensor ID (optional): </label>
             <select
